156f36ea7c3630b349a548e9fd3ac11e61a129e0
Liu, Xin <meteor1113@qq.com>
2025-02-13 12:32:01 +0000

    feat(rust): add file handling in greeting

--- rust/greeting/src/main.rs
+++ rust/greeting/src/main.rs
@@ -1,9 +1,13 @@
+use std::fs::File;
+
 fn main() {
     let a = 12;
     println!("a is {}", add(a, 4));
 
     println!("Hello, world!");
     let _s = String::from("ccc");
+
+    let _f = File::open("hello.txt");
 }
 
 fn add(a: i32, b: i32) -> i32 {
